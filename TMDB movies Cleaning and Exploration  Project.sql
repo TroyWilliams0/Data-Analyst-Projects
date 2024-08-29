77b114e29e5392d9fe25f541e3cdc96a3e7655c3
Troy Williams <troyw0745@gmail.com>
2024-08-29 16:08:43 -0400

    Update TMDB movies Cleaning and Exploration  Project.sql

--- TMDB movies Cleaning and Exploration  Project.sql
+++ TMDB movies Cleaning and Exploration  Project.sql
@@ -431,8 +431,8 @@ FROM movies3
 WHERE vote_count >= 1046
 ORDER BY vote_average DESC
 LIMIT 5;		
-				-- Using the average vote count the top 5 best vote averages are 
-				-- The Godfather with 8.70 out of 18,677 votes
+		-- Using the average vote count the top 5 best vote averages are 
+		-- The Godfather with 8.70 out of 18,677 votes
                 -- The Shawshank Redemption with 8.70 out of 24,649 votes
                 -- The Godfather Part II with 8.6 out of 11,293 votes
                 -- Schlinder's List with 8.57 out of 14,594 votes
@@ -450,10 +450,12 @@ MODIFY popularity float;
 SELECT DISTINCT runtime, COUNT(popularity) AS `Total Popularity`
 FROM movies3
 GROUP BY runtime
-ORDER BY `Total Popularity` DESC	-- There are 18,131 total popularity ratings
-LIMIT 5;							-- Doing this gives us the top 5 runtimes with the most popularity ratings
-									-- With a runtime of 90 there are 686 popularity ratings
-                                    -- With a runtime of 100 there are 574 popularity ratings
+ORDER BY `Total Popularity` DESC
+LIMIT 5;							
+				    -- There are 18,131 total popularity ratings
+				    -- Doing this gives us the top 5 runtimes with the most popularity ratings
+				    -- With a runtime of 90 there are 686 popularity ratings
+				    -- With a runtime of 100 there are 574 popularity ratings
                                     -- With a runtime of 95 there are 562 popularity ratings
                                     -- With a runtime of 97 there are 506 popularity ratings
                                     -- With a runtime of 93 there are 500 popularity ratings
@@ -462,8 +464,9 @@ LIMIT 5;							-- Doing this gives us the top 5 runtimes with the most popularit
 SELECT title, runtime, popularity
 FROM movies3
 ORDER BY popularity DESC
-LIMIT 5; 			-- Blue Beetle is the most popular with a rating of 2,994 and a runtime of 128
-					-- Gran Turismo is 2nd with a 2,681 rating and a runtime of 135
+LIMIT 5; 			
+		    -- Blue Beetle is the most popular with a rating of 2,994 and a runtime of 128
+		    -- Gran Turismo is 2nd with a 2,681 rating and a runtime of 135
                     -- The Nun II is 3rd with a popularity rating of 1,693 with a runtime of 110
                     -- Meg 2: The Trench is 4th with a popularity rating of 1,567 with a runtime of 116
                     -- Retribution is last with a rating of 1,547 and a runtime of 91
@@ -478,7 +481,7 @@ FROM movies3;
 SELECT *
 FROM movies3
 WHERE genres LIKE 'Action%';
-								-- Avatar is 1 making $2,923,706,026 of revenue in the action genre
+				-- Avatar is 1 making $2,923,706,026 of revenue in the action genre
                                 -- Spider-Man: No Way Home is 2 making $1,921,847,111 of revenue in action
                                 -- Jurassic World is 3 $1,671,537,444 of revenue in action
                                 -- Furious 7 is 4 making $1,515,341,399 of revenue in action 
@@ -489,7 +492,7 @@ SELECT *
 FROM movies3
 WHERE genres LIKE 'Adventure%'
 ORDER BY revenue DESC;
-								-- Avengers: Endgame has made the most revenue in the Adventure genre with $2,800,000,000 made
+				-- Avengers: Endgame has made the most revenue in the Adventure genre with $2,800,000,000 made
                                 -- Star Wars: The Force Awakens made the 2nd most revenue in the genre with $2,068,223,624 made
                                 -- Avengers: Infinity War made the 3rd most revenue in the genre with $2,052,415,039 made
                                 -- The Lion King made the 4th most revenue in the genre with $1,663,075,401 made
@@ -498,8 +501,9 @@ ORDER BY revenue DESC;
 SELECT *
 FROM movies3
 WHERE genres LIKE 'Fantasy%'
-ORDER BY revenue DESC; 			-- Harry Potter and the Deathly Hallows: Part 2 made the most revenue in the Fantasy genre making $1,341,511,219
-								-- The Hobbit: The Desolation of Smaug made the 2nd most revenue in the genre making $958,400,000
+ORDER BY revenue DESC; 		
+				-- Harry Potter and the Deathly Hallows: Part 2 made the most revenue in the Fantasy genre making $1,341,511,219
+				-- The Hobbit: The Desolation of Smaug made the 2nd most revenue in the genre making $958,400,000
                                 -- Doctor Strange in the Multiverse of Madness made the 3rd most in the genre making $955,775,804
                                 -- Spider-Man 3 made the 4th most in the genre making $894,983,373
                                 -- Spider-Man made the 5th most in the genre making $821,708,551
@@ -509,7 +513,7 @@ SELECT *
 FROM movies3
 WHERE genres LIKE 'Drama%'
 ORDER BY revenue DESC;
-								-- Titanic made the most revenue in the Drama genre making $2,264,162,353
+				-- Titanic made the most revenue in the Drama genre making $2,264,162,353
                                 -- The Dark Knight made the 2nd most in the genre making $1,004,558,444
                                 -- Oppenheimer made the 3rd most in the genre making $933,700,000
                                 -- Hi, Mom made the 4th most in the genre making $822,049,668
@@ -519,7 +523,7 @@ SELECT *
 FROM movies3
 WHERE genres LIKE 'Comedy%'
 ORDER BY revenue DESC;			
-								-- Barbie made the most revenue in the comedy genre making $1,428,545,028
+				-- Barbie made the most revenue in the comedy genre making $1,428,545,028
                                 -- Shrek Forever After made the 2nd most revenue making $752,600,867
                                 -- Forrest Gump made the 3rd most in the genre making $677,387,716
                                 -- Mamma Mia! made the 4th most in the genre making $609,841,637
@@ -530,7 +534,7 @@ SELECT *
 FROM movies3
 WHERE genres LIKE 'Romance%'
 ORDER BY revenue DESC;
-								-- Cinderella made the most revenue in the romance genre making $543,514,353
+				-- Cinderella made the most revenue in the romance genre making $543,514,353
                                 -- Beauty and the Beast made the 2nd most in the genre making $424,967,620
                                 -- There's Something About Mary made the 3rd most in the genre making $369,884,651
                                 -- Notting Hill made the 4th most in the genre making $363,889,678
@@ -541,7 +545,7 @@ SELECT *
 FROM movies3
 WHERE genres LIKE 'Animation%'
 ORDER BY revenue DESC;
-								-- The Super Mario Bros. Movie made the most in the animation genre making $1,355,725,263
+				-- The Super Mario Bros. Movie made the most in the animation genre making $1,355,725,263
                                 -- Frozen made the 2nd most in the genre making $1,274,219,009
                                 -- Toy Story 3 made the 3rd most in the genre making $1,066,969,703
                                 -- Zootopia made the 4th most in the genre making $1,023,784,195
@@ -552,7 +556,7 @@ SELECT *
 FROM movies3
 WHERE genres LIKE 'Family%'
 ORDER BY revenue DESC;
-								-- Frozen II made the most revenue in the family genre making $1,450,026,933
+				-- Frozen II made the most revenue in the family genre making $1,450,026,933
                                 -- Beauty and the Beast made the 2nd most in the genre making $1,266,115,964
                                 -- Minions made the 3rd most in the genre making $1,156,730,962
                                 -- Toy Story 4 made the 4th most in the genre making $1,073,394,593
@@ -564,7 +568,7 @@ FROM movies3
 WHERE genres LIKE 'Science Fiction%'
 ORDER BY revenue DESC;
 
-								-- Avatar: The Way of Water made the most revenue in the science fiction genre making $2,320,250,281
+				-- Avatar: The Way of Water made the most revenue in the science fiction genre making $2,320,250,281
                                 -- The Avengers made the 2nd most revenue in the genre making $1,518,815,515
                                 -- Transformers: Age of Extinction made the 3rd most revenue in the genre making $1,104,054,072
                                 -- Guardians of the Galaxy Vol. 2 made the 4th most revenue in the genre making $863,756,051
@@ -579,7 +583,7 @@ SELECT DISTINCT `Month`, COUNT(*) AS `Total Movies`
 FROM movies3
 GROUP BY `Month`
 ORDER BY `Month` ASC;
-						-- January had 1,187 movies released
+			-- January had 1,187 movies released
                         -- February had 1,295 movies released
                         -- March had 1,534 movies released
                         -- April had 1,324 movies released
@@ -615,7 +619,7 @@ SELECT DISTINCT `Month`, SUM(revenue) AS `Total Revenue`
 FROM movies3
 GROUP BY `Month`
 ORDER BY `Total Revenue` DESC;	-- December had the most revenue making $92,029,756,912
-								-- June was right behind with $88,287,537,900
+				-- June was right behind with $88,287,537,900
                                 -- July was behind that with $73,885,068,450
                                 -- May had $72,286,509,724 of total revenue
                                 -- November was 5th with $64,408,224,822 of total revenue made
@@ -660,15 +664,15 @@ SELECT DISTINCT(`Year`), SUM(`revenue`) AS `Total Revenue`
 FROM movies3
 GROUP BY `Year`
 ORDER BY `Total Revenue` DESC		-- 672,996,360,131
-LIMIT 5;							-- 2017 had the most revenue made with $31,417,632,932  
-									-- 2016 was 2nd with $31,276,758,929 in total revenue 
+LIMIT 5;			    -- 2017 had the most revenue made with $31,417,632,932  
+				    -- 2016 was 2nd with $31,276,758,929 in total revenue 
                                     -- 2018 was 3rd with $30,570,669,979 in total revenue
                                     -- 2019 was 4th with $30,323,376,769 in total revenue 
                                     -- 2015 was 5th with $29,571,459,995 in total revenue 
 
 -- 2017 accounts for 4.7% of all total revenue
 -- 2016 accounts for 4.6% of all total revenue
--- 2018 accounts for 4.54% of all total revenue
+-- 2018 accounts for 4.5% of all total revenue
 -- 2019 accounts for 4.5% of all total revenue
 -- 2015 accounts for 4.4% of all total revenue
 
@@ -684,7 +688,7 @@ SELECT DISTINCT(`Season`), COUNT(*) AS `Total Movies`
 FROM movies3
 GROUP BY `Season`
 ORDER BY `Total Movies` DESC;
-								-- Movies are more likely released in the Fall and Winter based on this data
+				-- Movies are more likely released in the Fall and Winter based on this data
                                 -- Fall had 5,534 movies released
                                 -- Winter had 4,274 movies released
                                 -- Spring had 4,205 movies released
@@ -696,7 +700,7 @@ SELECT DISTINCT(Season), SUM(revenue) AS `Total Revenue`
 FROM movies3
 GROUP BY Season
 ORDER BY `Total Revenue` DESC;	-- Based on revenue the movies released in the Summer are by far the money makers
-								-- For Summer the total revenue was $200,879,165,248 made
+				-- For Summer the total revenue was $200,879,165,248 made
                                 -- For Spring the total revenue was $167,014,503,027 made
                                 -- For Fall the total revenue was $153,530,155,185 made
                                 -- For Winter the total revenue was $151,572,536,671 made
@@ -707,8 +711,9 @@ ORDER BY `Total Revenue` DESC;	-- Based on revenue the movies released in the Su
 -- Spring revenue accounts for 24.8% of the total revenue
 -- Fall revenue accounts for 22.8% of the total revenue
 -- Winter revenue accounts for 22.6% of the total revenue
-															-- Summer is 5% more than the next best revenue which is Spring
-                                                            -- And it's 7% more than Fall and Winter
+							    
+-- Summer is 5% more than the next best revenue which is Spring 
+-- And it's 7% more than Fall and Winter
 
 
 ------------------------------------------------
@@ -728,16 +733,15 @@ FROM movies3
 WHERE `Year` BETWEEN '1960' AND '2000'
 ORDER BY revenue DESC
 LIMIT 10;								-- Titanic is number 1 making $2,264,162,353 in 1997
-										-- Star Wars: Episode I - The Phantom Menace is 2 making $924,317,558 in 1999
-										-- Jurassic Park is at 3 making $920,100,000 in 1993
-										-- Independence Day is number 4 making $817,400,891 in 1996
-										-- E.T. the Extra-Terrestrial is number 5 making $792,965,500 in 1982
-										-- Star Wars is number 6 making $775,398,007 in 1977
-										-- The Lion King is number 7 making $763,455,561 in 1994
-										-- Forrest Gump is number 8 making $677,387,716 in 1994
-										-- The Sixth Sense is number 9 making $672,806,292 in 1999
-										-- The Lost World: Jurassic Park is last making $618,638,999 in 1997
-
+									-- Star Wars: Episode I - The Phantom Menace is 2 making $924,317,558 in 1999
+									-- Jurassic Park is at 3 making $920,100,000 in 1993
+									-- Independence Day is number 4 making $817,400,891 in 1996
+									-- E.T. the Extra-Terrestrial is number 5 making $792,965,500 in 1982
+									-- Star Wars is number 6 making $775,398,007 in 1977
+									-- The Lion King is number 7 making $763,455,561 in 1994
+									-- Forrest Gump is number 8 making $677,387,716 in 1994
+									-- The Sixth Sense is number 9 making $672,806,292 in 1999
+									-- The Lost World: Jurassic Park is last making $618,638,999 in 1997
 
 
 
@@ -749,9 +753,9 @@ FROM movies3;
 SELECT title, revenue, `Year`
 FROM movies3
 WHERE `Year` BETWEEN '2000' AND '2010'
-ORDER BY revenue DESC					-- total top 5 revenue from 1960 and 2000 is $5,718,946,302
-LIMIT 5;								-- Avatar is number 1 in revenue making $2,923,706,026 in 2009
-										-- The Lord of the Rings: The Return of the King is number 2 making $1,118,888,979 in 2003
+ORDER BY revenue DESC			-- total top 5 revenue from 1960 and 2000 is $5,718,946,302
+LIMIT 5;				-- Avatar is number 1 in revenue making $2,923,706,026 in 2009
+					-- The Lord of the Rings: The Return of the King is number 2 making $1,118,888,979 in 2003
                                         -- Toy Story 3 is number 3 (ironic) making $1,066,969,703 in 2010
                                         -- Pirates of the Caribbean: Dead Man's Chest is number 4 making $1,065,659,812 in 2006
                                         -- Alice in Wonderland is number 5 also making $1,025,467,110 in 2010
@@ -770,8 +774,8 @@ SELECT title, revenue, `Year`
 FROM movies3
 WHERE `Year` BETWEEN '2010' AND '2024'
 ORDER BY revenue DESC				
-LIMIT 5;								-- Avengers: Endgame is number 1 making $2,800,000,000 in 2019
-										-- Avatar: The Way of Water is number 2 making $2,320,250,281 in 2022
+LIMIT 5;				-- Avengers: Endgame is number 1 making $2,800,000,000 in 2019
+					-- Avatar: The Way of Water is number 2 making $2,320,250,281 in 2022
                                         -- Star Wars: The Force Awakens is 3 making $2,068,223,624 in 2015
                                         -- Avengers: Infinity War is number 4 making $2,052,415,039 in 2018
                                         -- Spider-Man: No Way Home is last making $1,921,847,111 in 2021
@@ -800,7 +804,7 @@ SELECT title, production_companies, vote_average
 FROM movies3
 WHERE production_companies LIKE 'Paramount%';
 	
-										-- The 2 highest rated Paramount films are The Godfather with a rating of 8.71 
+					-- The 2 highest rated Paramount films are The Godfather with a rating of 8.71 
                                         -- and The Godfather Part II with a rating of 8.6
                                         
 SELECT DISTINCT production_companies, vote_average
@@ -812,7 +816,7 @@ FROM movies3
 WHERE production_companies LIKE 'Universal Pictures%'
 ORDER BY vote_average DESC;
 									
-										-- The 2 highest rated Universal Pictures films are Back to the Future with an 8.314 rating
+					-- The 2 highest rated Universal Pictures films are Back to the Future with an 8.314 rating
                                         -- and Gladiator with an 8.21 rating
                                         
 SELECT DISTINCT production_companies, vote_average
@@ -824,7 +828,7 @@ FROM movies3
 WHERE production_companies LIKE 'Warner Bros. Pictures%'
 ORDER BY vote_average DESC;
 
-										-- The 2 highest rated Warner Bros. Pictures films are Clouds with a rating of 8.30
+					-- The 2 highest rated Warner Bros. Pictures films are Clouds with a rating of 8.30
                                         -- and The Prestige with a rating of 8.203
                                         
 SELECT title, production_companies, vote_average, genres
@@ -832,7 +836,7 @@ FROM movies3
 WHERE production_companies LIKE 'Sony Pictures%' 
 ORDER BY vote_average DESC;
 
-										-- The 2 highest rated Sony Pictures films are Wish Dragon with a 8.00 rating
+					-- The 2 highest rated Sony Pictures films are Wish Dragon with a 8.00 rating
                                         -- and Inside Job with a 7.70 rating
 
 SELECT title, production_companies, vote_average
@@ -840,7 +844,7 @@ FROM movies3
 WHERE production_companies LIKE 'Lucasfilm%'
 ORDER BY vote_average DESC;
 
-										-- The 2 highest rated Lucasfilm films are The Empire Strikes Back with a rating of 8.4
+					-- The 2 highest rated Lucasfilm films are The Empire Strikes Back with a rating of 8.4
                                         -- and Star Wars with a rating of 8.2
 
 SELECT title, production_companies, vote_average
@@ -848,7 +852,7 @@ FROM movies3
 WHERE production_companies LIKE 'New Line Cinema%'
 ORDER BY vote_average DESC;
 
-										-- The 2 highest rated New Line Cinema films are The Lord of the Rings: The Return of the King with a rating of 8.5
+					-- The 2 highest rated New Line Cinema films are The Lord of the Rings: The Return of the King with a rating of 8.5
                                         -- and The Lord of the Rings: The Fellowship of the Ring with a rating of 8.4
 
 SELECT title, production_companies, vote_average
@@ -856,7 +860,7 @@ FROM movies3
 WHERE production_companies LIKE 'DC Comics%'
 ORDER BY vote_average DESC;
 
-										-- The 2 highest rated DC Comics films are The Dark Knight with a rating of 8.5
+					-- The 2 highest rated DC Comics films are The Dark Knight with a rating of 8.5
                                         -- and Batman: The Dark Knight Returns, Part 2 with a rating of 7.9
                                         
 ---------------------------------------------------
